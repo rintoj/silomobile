a41ba9e41ce73f6f0384769a9ca05111a6a8e3e2
Rinto Jose <rintoj@gmail.com>
2022-02-14 16:02:30 +0530

    Hide splash screen after the app is loaded

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -27,10 +27,17 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen'
 
+import SplashScreen from 'react-native-splash-screen'
+
 const Section: React.FC<{
   title: string
 }> = ({ children, title }) => {
   const isDarkMode = useColorScheme() === 'dark'
+
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <View style={styles.sectionContainer}>
       <Text
