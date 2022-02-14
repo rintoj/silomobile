e0867d47e0f6acf3797a9534bc937fa8af709671
Rinto Jose <rintoj@gmail.com>
2022-02-14 12:17:54 +0530

    fix lint

--- util/src/use-persisted-state.ts
+++ util/src/use-persisted-state.ts
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react'
-import { Storage } from './async-storage'
+import Storage from '@react-native-community/async-storage'
 
 export function usePersistedState<T>(
   key: string,
