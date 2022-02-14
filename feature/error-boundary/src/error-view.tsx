360f5282d9575ffa35a0ff3677e2511f7af900cd
Rinto Jose <rintoj@gmail.com>
2022-02-14 12:16:46 +0530

    add a sample feature and screen

---
+++ feature/error-boundary/src/error-view.tsx
@@ -0,0 +1,33 @@
+import { Text } from 'native-x-text'
+import { WarningIcon } from 'native-x-icon'
+import { Spacer } from 'native-x-spacer'
+import { Stack } from 'native-x-stack'
+import { COLOR } from 'native-x-theme'
+import React from 'react'
+
+export function ErrorView() {
+  return (
+    <Stack fill alignMiddle alignCenter>
+      <Stack
+        width={72}
+        height={72}
+        borderRadius='round'
+        padding='normal'
+        alignCenter
+        alignMiddle
+        backgroundColor={COLOR.ERROR}
+      >
+        <WarningIcon size={36} color={COLOR.PRIMARY} />
+        <Spacer size='xx-small' />
+      </Stack>
+      <Spacer />
+      <Text alignCenter bold fontSize={'x-large'}>
+        uh oh!
+      </Text>
+      <Spacer size='xx-small' />
+      <Stack padding='horizontal:large'>
+        <Text alignCenter>application crashed unexpectedly!</Text>
+      </Stack>
+    </Stack>
+  )
+}
