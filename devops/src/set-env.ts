5a0ecc118b998c55ab83e9a47b42d4d93548f7f1
Rinto Jose <rintoj@gmail.com>
2022-02-16 00:55:16 +0530

    configure set enviroment script

--- devops/src/set-env.ts
+++ devops/src/set-env.ts
@@ -1,50 +1,14 @@
-import { copy } from 'fs-extra'
 import { resolve } from 'path'
-import replace from 'replace'
-import { exec } from 'shelljs'
 import { writeFileWithTemplate } from './util'
 
 type Environment = 'prod' | 'dev' | 'local'
 
-function getLocalIP(environment: Environment) {
-  if (environment !== 'local') {
-    return
-  }
-  return exec("ifconfig | grep broadcast | head -1 | awk '{print $2}'").split('\n')[0]
-}
-
-async function copyFileOrDirectory(fileName: string, targetDir: string, environment: string) {
-  const configDir = resolve(__dirname, '../env', environment)
-  await copy(`${configDir}/${fileName}`, `${targetDir}/${fileName}`)
-}
-
-const appDir = resolve(__dirname, '../../mobile')
-const iosDir = resolve(appDir, 'ios')
-
-export async function configureEnvironment(environment: 'local' | 'dev' | 'prod') {
-  const localIP = getLocalIP(environment)
-  const configDir = resolve(__dirname, '..', '..', 'features', 'config', 'src')
+async function configureEnvironment(environment: Environment) {
+  const configDir = resolve(__dirname, '..', '..', 'feature', 'config', 'src')
   const configFile = resolve(configDir, `${environment}.json`)
   const targetFile = resolve(configDir, 'config.json')
-  await writeFileWithTemplate(configFile, targetFile, { localIP })
-  await copyFileOrDirectory('GoogleService-Info.plist', iosDir, environment)
-  await copyFileOrDirectory('Images.xcassets', resolve(iosDir, 'GM'), environment)
-  await copyFileOrDirectory('AppCenter-Config.plist', iosDir, environment)
-  await copyFileOrDirectory('splash-icon.png', resolve(iosDir, 'GM'), environment)
-  await updateIOSBundleId(environment)
+  console.log({ configFile, targetFile })
+  await writeFileWithTemplate(configFile, targetFile, {})
 }
 
-async function updateIOSBundleId(environment: Environment) {
-  const currentEnv = environment === 'prod' ? 'prod' : 'dev'
-  const alternateEnv = environment === 'prod' ? 'dev' : 'prod'
-  const files = [
-    resolve(iosDir, 'GM.xcodeproj', 'project.pbxproj'),
-    resolve(iosDir, 'GM', 'Info.plist'),
-    resolve(iosDir, 'fastlane', 'Appfile'),
-  ]
-  replace({
-    regex: new RegExp(`xyz.saygm.${alternateEnv}`, 'g'),
-    replacement: `xyz.saygm.${currentEnv}`,
-    paths: files,
-  })
-}
+configureEnvironment(process.argv[2] as any)
