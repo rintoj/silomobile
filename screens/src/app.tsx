d62780dd15922c1a224c9aa66347dd079a94d9d1
Rinto Jose <rintoj@gmail.com>
2022-02-15 18:51:21 +0530

    configure theme

---
+++ screens/src/app.tsx
@@ -0,0 +1,16 @@
+import { THEMES } from '@silo-feature/theme'
+import { THEME, ThemeProvider } from 'native-x-theme'
+import React from 'react'
+import { StatusBar } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+
+export function App() {
+  React.useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+  return (
+    <ThemeProvider theme={THEME.LIGHT} themes={THEMES} autoSwitchTheme={false}>
+      <StatusBar barStyle='light-content' animated translucent />
+    </ThemeProvider>
+  )
+}
