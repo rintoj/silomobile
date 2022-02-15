bc0c336becd039eb330ce898fb4bd72501dee77c
Rinto Jose <rintoj@gmail.com>
2022-02-15 18:55:47 +0530

    fix: project configuration and ts config

--- custom.d.ts
+++ custom.d.ts
@@ -3,6 +3,11 @@ declare module '*.png' {
   export default value
 }
 
+declare module '*.jpg' {
+  const value: any
+  export default value
+}
+
 declare module '*.svg' {
   import React from 'react'
   import { SvgProps } from 'react-native-svg'
