400aedf4d95811a7a3c688e331d61dacdefb0a38
Aswin S <mr.aswin@gmail.com>
2022-02-16 03:55:16 +0530

    feat: add code scanner component

---
+++ feature/code-scanner/src/code-scanner.tsx
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react'
+import { BarCodeReadEvent, RNCamera } from 'react-native-camera'
+import QRCodeScanner from 'react-native-qrcode-scanner'
+
+const styles = {
+  camera: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center' as const,
+    justifyContent: 'center' as const,
+  },
+}
+
+interface Props {
+  onScanSuccess?: (data: string) => void
+}
+
+export function CodeScanner({ onScanSuccess }: Props) {
+  const handleOnRead = useCallback(
+    (e: BarCodeReadEvent) => {
+      if (!e.data) {
+        return
+      }
+      onScanSuccess?.(e.data)
+    },
+    [onScanSuccess],
+  )
+
+  return (
+    <QRCodeScanner
+      flashMode={RNCamera.Constants.FlashMode.auto}
+      cameraStyle={styles.camera}
+      onRead={handleOnRead}
+    />
+  )
+}
