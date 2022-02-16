80abf595c08447084e00712a5a81d4b10bcb2964
Aswin S <mr.aswin@gmail.com>
2022-02-17 01:37:18 +0530

    fix: prevent background image resize on keyboard open

--- component/background/src/background.tsx
+++ component/background/src/background.tsx
@@ -4,11 +4,15 @@ import React from 'react'
 import { ImageBackground } from 'react-native'
 import { styles as s } from 'tachyons-react-native'
 import background from './images/background.jpg'
+import { Dimensions } from 'react-native'
 
 interface Props {
   children?: React.ReactChild
   onLoad?: () => void
 }
+
+const { width, height } = Dimensions.get('screen')
+const style = { flex: 1, width, height }
 
 export function Background({ children, onLoad }: Props) {
   return (
@@ -16,7 +20,7 @@ export function Background({ children, onLoad }: Props) {
       fadeDuration={0}
       source={background}
       resizeMode='cover'
-      style={s.flex}
+      style={style}
       onLoadEnd={onLoad}
     >
       <Stack backgroundColor={COLOR_X.ACCENT3} opacity='low' style={s.absolute as any} fill />
