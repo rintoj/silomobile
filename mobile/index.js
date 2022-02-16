c7bcd0869c32b12e8692edb5cae3d19e25feef17
Aswin S <mr.aswin@gmail.com>
2022-02-17 00:11:36 +0530

    fix: prevent rn-gesture-handler warning

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,10 @@
 import { App } from '@silo/screens'
 import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
+import { LogBox } from 'react-native'
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+])
 
 AppRegistry.registerComponent(appName, () => App)
