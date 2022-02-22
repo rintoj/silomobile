4431270b3e11318bb8a0117be990a73ae73270d3
Rinto Jose <rintoj@gmail.com>
2022-02-23 03:19:44 +0530

    Refactor/navigation (#14)
    * refactor: home screen navigation
    
    * refactor: lot details screen
    
    * refactor: redefine screens
    
    * refactor: PurchaseOrderScreen navigation

---
+++ screens/src/home/home-stack.tsx
@@ -0,0 +1,40 @@
+import { useNavigation } from '@react-navigation/native'
+import {
+  createStackNavigator,
+  StackNavigationOptions,
+  TransitionPresets,
+} from '@react-navigation/stack'
+import { AppHeader } from '@silo-feature/app-header'
+import { NavBar } from '@silo-feature/nav-bar'
+import React from 'react'
+import { LotDetailsScreen } from '../lot-details/lot-details-screen'
+import { Modals } from '../navigation/modals'
+import { Screens } from '../navigation/screens'
+import { PurchaseOrderScreen } from '../purchase-order/purchase-order-screen'
+import { HomeScreen } from './home-screen'
+
+const { Navigator, Screen } = createStackNavigator()
+
+const screenOptions: StackNavigationOptions = {
+  headerShown: false,
+  animationEnabled: true,
+  ...TransitionPresets.SlideFromRightIOS,
+}
+
+export function HomeStack() {
+  const { navigate } = useNavigation<any>()
+  const openCodeScanner = React.useCallback(() => {
+    navigate(Modals.CodeScanner)
+  }, [navigate])
+  return (
+    <>
+      <AppHeader />
+      <Navigator screenOptions={screenOptions}>
+        <Screen name={Screens.Home} component={HomeScreen} />
+        <Screen name={Screens.PurchaseOrder} component={PurchaseOrderScreen} />
+        <Screen name={Screens.LotDetails} component={LotDetailsScreen} />
+      </Navigator>
+      <NavBar onScanIconTap={openCodeScanner} />
+    </>
+  )
+}
--- screens/src/purchase-order/purchase-order-screen.tsx
+++ screens/src/purchase-order/purchase-order-screen.tsx
@@ -2,8 +2,6 @@ import { useNavigation } from '@react-navigation/native'
 import { PageHeader } from '@silo-component/page-header'
 import { Screen } from '@silo-component/screen'
 import { Text } from '@silo-component/text'
-import { AppHeader } from '@silo-feature/app-header'
-import { NavBar } from '@silo-feature/nav-bar'
 import { PurchaseOrder } from '@silo-feature/purchase-order'
 import { COLOR_X } from '@silo-feature/theme'
 import { Spacer } from 'native-x-spacer'
@@ -19,25 +17,21 @@ export function PurchaseOrderScreen() {
   const navigateToLotDetails = React.useCallback(() => navigate(Screens.LotDetails), [navigate])
 
   return (
-    <>
-      <AppHeader />
-      <Screen withSafeArea>
-        <Stack fill backgroundColor={COLOR_X.PAGE}>
-          <Spacer />
-          <Spacer size='small' />
-          <PageHeader showBackButton accentColor={COLOR.SUCCESS} onTapLeftButton={navigateToHome}>
-            <Stack horizontal alignMiddle fill alignCenter>
-              <PurchaseOrderIcon />
-              <Spacer size='small' />
-              <Text semiBold fontSize='x-large' textColor={COLOR.PRIMARY}>
-                PO # 65444
-              </Text>
-            </Stack>
-          </PageHeader>
-          <PurchaseOrder onSelectLot={navigateToLotDetails} />
-          <NavBar />
-        </Stack>
-      </Screen>
-    </>
+    <Screen withSafeArea>
+      <Stack fill backgroundColor={COLOR_X.PAGE}>
+        <Spacer />
+        <Spacer size='small' />
+        <PageHeader showBackButton accentColor={COLOR.SUCCESS} onTapLeftButton={navigateToHome}>
+          <Stack horizontal alignMiddle fill alignCenter>
+            <PurchaseOrderIcon />
+            <Spacer size='small' />
+            <Text semiBold fontSize='x-large' textColor={COLOR.PRIMARY}>
+              PO # 65444
+            </Text>
+          </Stack>
+        </PageHeader>
+        <PurchaseOrder onSelectLot={navigateToLotDetails} />
+      </Stack>
+    </Screen>
   )
 }
