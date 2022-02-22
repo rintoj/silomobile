d5f5eec3ef3a64f52994cc3841bb56a01b199a88
Aswin S <mr.aswin@gmail.com>
2022-02-23 01:44:51 +0530

    feat: check for updates on startup & resume

--- mobile/index.js
+++ mobile/index.js
@@ -1,9 +1,10 @@
 import { App } from '@silo/screens'
 import { AppRegistry, LogBox } from 'react-native'
 import { name as appName } from './app.json'
+import { withCodePush } from '@silo-feature/code-push'
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
 ])
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, withCodePush(App))
