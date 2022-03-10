b89ed72a3e536037de459c8e322431e839af370d
Aswin S <mr.aswin@gmail.com>
2022-03-11 03:48:20 +0530

    fix: homescreen layout issues

--- screens/src/home/home-stack.tsx
+++ screens/src/home/home-stack.tsx
@@ -1,21 +1,18 @@
-import { useNavigation } from '@react-navigation/native'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import {
   createStackNavigator,
   StackNavigationOptions,
   TransitionPresets,
 } from '@react-navigation/stack'
-import { AppHeader } from '@silo-feature/app-header'
-import { NavBar } from '@silo-feature/nav-bar'
+import { Stack } from 'native-x-stack'
 import React from 'react'
 import SplashScreen from 'react-native-splash-screen'
 import { LotDetailsScreen } from '../lot-details/lot-details-screen'
-import { Modals } from '../navigation/modals'
 import { Screens } from '../navigation/screens'
 import { PurchaseOrderScreen } from '../purchase-order/purchase-order-screen'
 import { HomeScreen } from './home-screen'
 
 const { Navigator, Screen } = createStackNavigator()
-
 const screenOptions: StackNavigationOptions = {
   headerShown: false,
   animationEnabled: true,
@@ -23,22 +20,16 @@ const screenOptions: StackNavigationOptions = {
 }
 
 export function HomeStack() {
-  const { navigate } = useNavigation<any>()
-  const openCodeScanner = React.useCallback(() => {
-    navigate(Modals.CodeScanner)
-  }, [navigate])
-
+  const tabBarHeight = useBottomTabBarHeight()
   React.useEffect(() => SplashScreen.hide(), [])
-
   return (
-    <>
-      <AppHeader />
+    <Stack fill>
       <Navigator screenOptions={screenOptions}>
         <Screen name={Screens.Home} component={HomeScreen} />
         <Screen name={Screens.PurchaseOrder} component={PurchaseOrderScreen} />
         <Screen name={Screens.LotDetails} component={LotDetailsScreen} />
       </Navigator>
-      <NavBar onScanIconTap={openCodeScanner} />
-    </>
+      <Stack height={tabBarHeight} />
+    </Stack>
   )
 }
