6dd4dbd200f1cae3dc0f0fd89b3e1d3e2ac9df67
Aswin S <mr.aswin@gmail.com>
2022-04-14 21:53:02 +0530

    fix: reanimated config for android

--- mobile/android/app/src/main/java/com/usesilo/mobile/stage/MainApplication.java
+++ mobile/android/app/src/main/java/com/usesilo/mobile/stage/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.bridge.JSIModulePackage; 
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -39,6 +41,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSBundleFile() {
             return CodePush.getJSBundleFile();
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
