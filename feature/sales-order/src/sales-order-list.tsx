64c1656e3f80b4c09058682b6b4d05dfb68c70cd
Aswin S <mr.aswin@gmail.com>
2022-04-20 02:29:12 +0530

    feat: add search and filter options for sale order

--- feature/sales-order/src/sales-order-list.tsx
+++ feature/sales-order/src/sales-order-list.tsx
@@ -1,23 +1,49 @@
+import { Button } from '@silo-component/button'
 import { DataView } from '@silo-component/data-view'
+import { FilterIcon, SearchIcon } from '@silo-component/icons'
 import { Text } from '@silo-component/text'
 import { COLOR_X } from '@silo-feature/theme'
 import { Spacer } from 'native-x-spacer'
 import { Stack } from 'native-x-stack'
 import { Tappable } from 'native-x-tappable'
+import { COLOR } from 'native-x-theme'
 import React from 'react'
 import { FlatList } from 'react-native'
 import { EmptyView } from './empty-view'
 import { SalesOrderListItemView } from './sales-order-list-item-view'
 import { SalesOrderSearchResult } from './use-sales-order-search-query'
-
 interface Props {
+  searchActive?: boolean
   orders?: Array<SalesOrderSearchResult>
   loading?: boolean
   error?: Error | null
   onSelect?: (id: number) => void
+  onSearchIconTap?: () => void
+  onClearSearchTap?: () => void
+  onFilterIconTap?: () => void
 }
 
-export function SalesOrderList({ orders, loading, error, onSelect }: Props) {
+export function SalesOrderList({
+  orders,
+  loading,
+  error,
+  searchActive,
+  onSelect,
+  onClearSearchTap,
+  onSearchIconTap,
+  onFilterIconTap,
+}: Props) {
+  const renderHeader = React.useCallback(
+    () => (
+      <Header
+        searchActive={searchActive}
+        onSearch={onSearchIconTap}
+        onFilter={onFilterIconTap}
+        onClearSearchTap={onClearSearchTap}
+      />
+    ),
+    [searchActive, onClearSearchTap, onFilterIconTap, onSearchIconTap],
+  )
   const renderItem = React.useCallback(
     ({ item }: { item: SalesOrderSearchResult }) => (
       <Tappable data={item.salesOrderID} onTap={onSelect}>
@@ -31,12 +57,12 @@ export function SalesOrderList({ orders, loading, error, onSelect }: Props) {
   )
   return (
     <Stack fill>
-      {!orders ? <Header /> : null}
+      {!orders ? renderHeader() : null}
       <DataView fill isLoading={loading} error={error} data={orders} emptyMessage={<EmptyView />}>
         <FlatList
           data={orders}
           renderItem={renderItem}
-          ListHeaderComponent={<Header />}
+          ListHeaderComponent={renderHeader()}
           ListFooterComponent={<Spacer size='large' />}
         />
       </DataView>
@@ -44,15 +70,47 @@ export function SalesOrderList({ orders, loading, error, onSelect }: Props) {
   )
 }
 
-function Header() {
+function Header({
+  searchActive,
+  onSearch,
+  onFilter,
+  onClearSearchTap,
+}: {
+  searchActive?: boolean
+  onClearSearchTap?: () => void
+  onSearch?: () => void
+  onFilter?: () => void
+}) {
   return (
     <Stack padding='normal' fillHorizontal>
       <Spacer size='xx-small' />
-      <Text fill fontSize='x-large' textColor={COLOR_X.ACCENT3}>
-        Outgoing
-      </Text>
+      <Stack alignMiddle horizontal fillHorizontal>
+        <Text fontSize='x-large' textColor={COLOR_X.ACCENT3}>
+          {searchActive ? 'Results' : 'Outgoing'}
+        </Text>
+        <Spacer fill />
+        {searchActive ? (
+          <Button clear textColor={COLOR.TERTIARY} size='small' onTap={onClearSearchTap}>
+            Clear Search
+          </Button>
+        ) : null}
+        <Tappable onTap={onSearch}>
+          <SearchIcon />
+        </Tappable>
+
+        {!searchActive ? (
+          <>
+            <Spacer />
+            <Spacer size='x-small' />
+            <Tappable onTap={onFilter}>
+              <FilterIcon />
+            </Tappable>
+          </>
+        ) : null}
+        <Spacer size='x-small' />
+      </Stack>
       <Spacer size='small' />
-      <Text textColor={COLOR_X.ACCENT2}>List of SOs</Text>
+      <Text textColor={COLOR_X.ACCENT2}>{searchActive ? 'Search Results' : 'List of SOs'}</Text>
     </Stack>
   )
 }
--- screens/src/outgoing-orders/outgoing-orders-home.tsx
+++ screens/src/outgoing-orders/outgoing-orders-home.tsx
@@ -1,3 +1,4 @@
+import { RouteProp, useRoute } from '@react-navigation/core'
 import type { StackScreenProps } from '@react-navigation/stack'
 import { Screen } from '@silo-component/screen'
 import { SalesOrders } from '@silo-feature/sales-order'
@@ -7,19 +8,52 @@ import { Stack } from 'native-x-stack'
 import { COLOR } from 'native-x-theme'
 import React from 'react'
 import { StatusBar } from 'react-native'
+import { Modals } from '../navigation/modals'
 import { Screens } from '../navigation/screens'
 
-export function OutgoingOrdersHomeScreen({ navigation }: StackScreenProps<any>) {
-  const navigateToSalesOrderDetails = (id: number) => {
-    navigation.navigate(Screens.OutgoingOrdersTab, { screen: Screens.SalesOrder, params: { id } })
+type OutgoingOrdersHomeParamList = {
+  [Screens.Home]: {
+    id?: string
+    invoiceNumber?: string
+    purchaseOrderNumber?: string
   }
+}
+
+export function OutgoingOrdersHomeScreen({ navigation }: StackScreenProps<any>) {
+  const { params } = useRoute<RouteProp<OutgoingOrdersHomeParamList>>()
+  const { id, invoiceNumber, purchaseOrderNumber } = params ?? {}
+  const navigateToSearch = () => {
+    navigation.navigate(Modals.Search, { target: Screens.OutgoingOrdersHome })
+  }
+  const navigateToFilters = () => {
+    navigation.navigate(Modals.Filters)
+  }
+  const clearSearchParams = () => {
+    navigation.navigate(Screens.OutgoingOrdersHome)
+  }
+
+  const navigateToSalesOrderDetails = (orderId: number) => {
+    navigation.navigate(Screens.OutgoingOrdersTab, {
+      screen: Screens.SalesOrder,
+      params: { id: orderId },
+    })
+  }
+
   return (
     <Screen>
       <StatusBar barStyle='light-content' backgroundColor='#235039' animated />
       <Stack fill backgroundColor={COLOR.PRIMARY}>
         <Stack alignCenter fill backgroundColor={COLOR_X.PAGE}>
           <Spacer size='large' />
-          <SalesOrders onSelect={navigateToSalesOrderDetails} />
+          <SalesOrders
+            id={id}
+            onSelect={navigateToSalesOrderDetails}
+            invoiceNumber={invoiceNumber}
+            purchaseOrderNumber={purchaseOrderNumber}
+            onClearSearchTap={clearSearchParams}
+            onSearchIconTap={navigateToSearch}
+            onFilterIconTap={navigateToFilters}
+          />
         </Stack>
       </Stack>
     </Screen>
